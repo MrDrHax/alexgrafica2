2919098a6045001c5f362b879f3261dfd544c9a1
MrDrHax <alexfh2001@gmail.com>
2023-11-22 16:02:43 -0600

    Add all agents Added space for A*

--- sim/agents.py
+++ sim/agents.py
@@ -1,20 +1,123 @@
+from typing import Any
 from fastautomata import Agents
+from fastautomata.Board import SimulatedBoard as Board
+from fastautomata.ClassTypes import Pos
+from fastautomata.fastautomata_clib import BaseAgent
+import random
+from enum import Enum
 
 # TODO hacer que todos los agentes se pongan juntos de un jalon
 
-class Road(Agents.StaticAgent):
+class Direction(Enum):
+    UP = "^"
+    DOWN = "v"
+    LEFT = "<"
+    RIGHT = ">"
+    FUCKING_CRAZY = "O"
+
+class Neighbors(Enum):
+    Top_Left        = 0
+    Top_Center      = 1
+    Top_Right       = 2
+    Middle_Left     = 3
+    Middle_Center   = 4
+    Middle_Right    = 5
+    Bottom_Left     = 6
+    Bottom_Center   = 7
+    Bottom_Right    = 8
+    
+
+destinationsList: list['Destination'] = []
+roadsList: list['Road'] = []
+
+class Road(Agents.SimulatedAgent):
     roads: list['Road'] = []
+    direction: Direction
+
+    def __init__(self, board: Board, pos: Pos, direction: Direction):
+        super().__init__(board, pos, "Road", 0, False)
+        self.direction = direction
+        roadsList.append(self)
+    
+    def step(self) -> None:
+        pass
+
+    def calculateRoads(self):
+        neighbors = self.get_neighbors(1, False)
+        options: list[BaseAgent] = []
+
+        match self.direction:
+            # si queremos agregar en un futuro que no solo sea diagonal... se puede aqui
+            case Direction.UP:
+                options = [
+                    neighbors[Neighbors.Top_Left],
+                    neighbors[Neighbors.Top_Center],
+                    neighbors[Neighbors.Top_Right],
+                ]
+            case Direction.LEFT:
+                options = [
+                    neighbors[Neighbors.Top_Left],
+                    neighbors[Neighbors.Middle_Left],
+                    neighbors[Neighbors.Bottom_Left],
+                ]
+            case Direction.RIGHT:
+                options = [
+                    neighbors[Neighbors.Top_Right],
+                    neighbors[Neighbors.Middle_Right],
+                    neighbors[Neighbors.Bottom_Right],
+                ]
+            case Direction.DOWN:
+                options = [
+                    neighbors[Neighbors.Bottom_Left],
+                    neighbors[Neighbors.Bottom_Center],
+                    neighbors[Neighbors.Bottom_Right],
+                ]
+        
+        for option in options:
+            if option is not None:
+                if option.type == "Road":
+                    self.roads.append(option)
+
+    def cookRoutes(self):
+        # TODO @Alex-Barr0n agrega aquí tu A*
+        # has que valla a cualquier Destination (para que no tenga que calcular el a* cada vez que se mueva) 
+        # puedes usar el destinationList e iterar en el
+        pass 
+
+    def getRoute(self, destination: 'Destination') -> list[Pos]:
+        # TODO @Alex-Barr0n agrega aquí la lectura de tu A*
+        pass
 
 class Destination(Agents.StaticAgent):
-    pass
+    def __init__(self, board: Board, pos: Pos):
+        super().__init__(board, pos, "Destination", 0, False)
+        destinationsList.append(self)
 
 class Building(Agents.StaticAgent):
-    pass
+    def __init__(self, board: Board, pos: Pos):
+        super().__init__(board, pos, "Building", 0, False)
 
 class Stoplight(Agents.simulatedAgent):
+    def __init__(self, board: Board, pos: Pos):
+        super().__init__(board, pos, "Stoplight_go", 2, False)
+
     def step(self):
         pass
 
 class Car(Agents.simulatedAgent):
+    destination: Destination
+    def __init__(self, board: Board):
+        choices = [
+            # corners of the map
+            Pos(0, 0),
+            Pos(0, board.specialValues["board_height"] - 1),
+            Pos(board.specialValues["board_width"] - 1, 0),
+            Pos(board.specialValues["board_width"] - 1, board.specialValues["board_height"] - 1)
+        ]
+        spawnIn = random.choice(choices)
+        super().__init__(board, spawnIn, "Car", 1, False)
+
+        self.destination = random.choice(destinationsList)
+
     def step(self):
         pass
--- sim/boardMan.py
+++ sim/boardMan.py
@@ -19,8 +19,10 @@ def createBoard(data: dict) -> Board.SimulatedBoard:
 
     # TODO leer desde el mapa
 
-    # data["board_width"] = 
-    # data["board_height"] = 
+    with open(data["map"], "r") as f:
+        map = f.readlines()
+        data["board_width"] = len(map[0])
+        data["board_height"] = len(map)
 
     board = Board.SimulatedBoard(data["board_width"], data["board_height"], 3)
 
@@ -38,6 +40,11 @@ def createBoard(data: dict) -> Board.SimulatedBoard:
 
     # add types
     board.addColor("Road", (50, 50, 50))
+    board.addColor("Building", (0, 0, 0))
+    board.addColor("Stoplight_go", (0, 255, 0))
+    board.addColor("Stoplight_stop", (255, 0, 0))
+    board.addColor("Car", (0, 0, 255))
+    board.addColor("Destination", (255, 255, 0))
 
     # add board variables
     obstacles = int(data["board_height"] * data["board_width"] * data["obstacles"])
@@ -47,12 +54,15 @@ def createBoard(data: dict) -> Board.SimulatedBoard:
 
     # collisions
     # car to stoplight
-    board.layer_collisions.addCollision(fastautomata_clib.CollisionType.SOLID, 1, 2)
+    board.layer_collisions.addCollision(fastautomata_clib.CollisionType.TRIGGER, 1, 2)
     # car to map
     board.layer_collisions.addCollision(fastautomata_clib.CollisionType.TRIGGER, 1, 0)
 
     # add agents
     board.append_on_reset(addAgents)
+
+    # add the car summoner
+    board.step_instructions_add(spawnCar)
 
     if data["drawer"]:
         return board, drawer
@@ -64,4 +74,30 @@ def addAgents(board: Board.SimulatedBoard):
     Add the agents to the board.
     '''
 
-    # TODO leer el mapa
+    with open(board.specialValues["map"], "r") as f:
+        mapB = f.readlines()
+
+        for row in range(len(mapB)):
+            for col in range(len(mapB[row])):
+                pos = Pos(col, row)
+                match mapB[row][col]:
+                    case "#":
+                        LocalAgents.Building(board, pos)
+                    case "D":
+                        LocalAgents.Destination(board, pos)
+                    case "S", "s":
+                        LocalAgents.Stoplight(board, pos)
+                    case "<", ">", "^", "v":
+                        LocalAgents.Road(board, pos, mapB[row][col])
+    
+    for road in LocalAgents.roadsList:
+        road.cookRoutes()
+
+count = 0
+
+def spawnCar(board: Board.SimulatedBoard):
+    global count
+    count += 1
+    if count > 5:
+        LocalAgents.Car(LocalAgents.board)
+        count = 0
